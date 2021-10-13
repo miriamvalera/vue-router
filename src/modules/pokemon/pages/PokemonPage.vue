89801085ce5d7232406bb87fe5f3e055c655e7be
Miriam Valera <miriamvdaw@gmail.com>
2021-10-13 15:49:13 +0200

    add query param into url, show image of pokemon and control error in pokemon request

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>Pokemon: <span>#{{ id }}</span> </h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    </div>
 </template>
 
 <script>
@@ -13,7 +16,8 @@ export default {
 
     data() {
         return {
-            //id: null
+            //id: this.$route.params.id,
+            pokemon: null
         }
     },
 
@@ -21,8 +25,26 @@ export default {
         // const { id } = this.$route.params
         // console.log(id)
         // this.id = id
-
         // console.log(this.$attrs)
+        this.getPokemon()
+    },
+
+    methods: {
+        async getPokemon() {
+            try {
+                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ this.id }`).then( r => r.json() );
+                console.log(pokemon)
+                this.pokemon = pokemon
+            }catch (error) {
+                this.$router.push('/')
+                console.log('No hay nada que hacer aquí')
+            }            
+        }
+    },
+    watch: {
+        id() {
+            this.getPokemon()
+        }
     }
 }
 </script>
