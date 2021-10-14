937eef715ab5860fb7f6a07064b0ce209351145c
Miriam Valera <miriamvdaw@gmail.com>
2021-10-14 12:46:53 +0200

    modify our Navbar with dbz

--- src/modules/shared/components/CustomLink.vue
+++ src/modules/shared/components/CustomLink.vue
@@ -6,12 +6,11 @@
 
     <router-link
         v-else
-        :to="link.to"
-        v-slot="{ href, isActive }"
+        :to="route"
+        v-slot="{ isActive }"
     >
-        <a :href="href"
-            :class=" isActive ? 'is-active' : 'normal-link'"
-        >
+        <!-- href, isActive -->
+        <a :class=" isActive ? 'is-active' : 'normal-link'">
             {{ link.name }}
 
         </a>
@@ -19,7 +18,6 @@
 </template>
 
 <script>
-import { computed } from '@vue/reactivity'
 
 export default {
     props: {
@@ -31,6 +29,11 @@ export default {
     computed: {
         isExternalLink() {
             return this.link.to.startsWith('http')
+        },
+        route() {
+            return this.link.id === undefined
+                ? { name: this.link.to }
+                : { name: this.link.to, params: { id: this.link.id } }
         }
     }
 }
