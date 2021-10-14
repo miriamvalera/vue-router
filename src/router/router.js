8224cf17c76d689491e612a08de0bb5fe5da381a
Miriam Valera <miriamvdaw@gmail.com>
2021-10-14 11:41:38 +0200

    add PokemonLayout with multiple children routes

--- src/router/router.js
+++ src/router/router.js
@@ -3,28 +3,40 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
     {
         path: '/',
-        redirect: '/home'
+        redirect: '/pokemon'
     },
     {
-        path: '/home', 
-        name: 'home',
-        component: () => import(/* webpackChunkName: "ListPage" */'../modules/pokemon/pages/ListPage') 
+        path: '/pokemon',
+        name: 'pokemon',
+        component: () => import(/* webpackChunkName: "PokemonLayout" */'../modules/pokemon/layouts/PokemonLayout'),
+        children: [
+            {
+                path: 'home', 
+                name: 'pokemon-home',
+                component: () => import(/* webpackChunkName: "ListPage" */'../modules/pokemon/pages/ListPage') 
+            },
+            {
+                path: 'about', 
+                name: 'pokemon-about',
+                component: () => import(/* webpackChunkName: "AboutPage" */'../modules/pokemon/pages/AboutPage') 
+            },
+            {
+                path: 'pokemonid/:id', 
+                name: 'pokemon-id',
+                component: () => import(/* webpackChunkName: "PokemonPage" */'../modules/pokemon/pages/PokemonPage'),
+                props: ( route ) => {
+                    // const { id } = route.params
+                    const id = Number( route.params.id )
+                    return isNaN( id ) ?  { id: 1 } : { id }
+                } 
+            },
+            {
+                path: '',
+                redirect: { name: 'pokemon-about' }
+            },
+        ] 
     },
-    {
-        path: '/about', 
-        name: 'about',
-        component: () => import(/* webpackChunkName: "AboutPage" */'../modules/pokemon/pages/AboutPage') 
-    },
-    {
-        path: '/pokemonid/:id', 
-        name: 'pokemon-id',
-        component: () => import(/* webpackChunkName: "PokemonPage" */'../modules/pokemon/pages/PokemonPage'),
-        props: ( route ) => {
-            // const { id } = route.params
-            const id = Number( route.params.id )
-            return isNaN( id ) ?  { id: 1 } : { id }
-        } 
-    },
+    
     {
         path: '/:pathMatch(.*)*', 
         component:  () => import(/* webpackChunkName: "NoPageFound" */'../modules/shared/pages/NoPageFound') 
